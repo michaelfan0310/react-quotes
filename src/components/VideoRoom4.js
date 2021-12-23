c46ef5554555e7fe57656e245c559a17b0e1079f
michaelfan0310 <michaelfan0310@gmail.com>
2021-12-22 23:41:52 -0500

    swithCardBack

--- src/components/VideoRoom4.js
+++ src/components/VideoRoom4.js
@@ -1,20 +1,45 @@
-import React from "react";
 
-import video from '../Video/1900.mp4';
+import React, {useState} from "react";
 
-function VideoRoom4(){
+import video1 from '../Video/1900.mp4';
+import video2 from '../Video/19002.mp4';
+
+
+function VideoRoom4(){  
+
+    const videoRef = React.useRef();
+
+React.useEffect(() => {
+  if (!video2.current) {
+    return
+  
+//    const newVideo=video2;
+    // video everytime it changes
+   (video2.load());
+} [videoRef]},)
+
+    const [video, setVideo]=useState(video1);    
+function switchVideo(){    
+       
+       setVideo(video2);
+    }; 
+
+ 
     return(
         <div className="dictionary2">
-        <div className="card2">
-        
+        <div className="card2">        
 
-       <video controls autoPlay loop muted className="video3">
+       <video controls autoPlay loop muted className="video3" >
           <source src={video} type="video/mp4" >
           </source>
          <source src="movie.ogg" type="video/ogg">
         </source>
         </video>
-       
+        <button 
+        onClick={switchVideo}
+         
+        id="vbutton" >Switch</button>
+        
         </div>
         </div>
     );
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -12,6 +12,20 @@ function Card(props){
         const quoteNew=props.quote2;
         setQuote(quoteNew);        
     }
+
+    function switchCardBack(){
+        
+        setQuote(quoteOriginal);        
+    }
+  
+// const [isMousedOver, setMouseOver] = useState(false);
+
+//  function handleMouseOver() {
+//     setMouseOver(true);
+//   }
+//    function handleMouseOut() {
+//     setMouseOver(false);
+//   }
 return (
     <div>
        <dl className="dictionary">
@@ -20,7 +34,19 @@ return (
             <dt>
                 <h2 className="name">{props.name}</h2></dt>
                 <Avatar img={props.img}/>
-                <button onClick={switchCard}>Switch</button>
+                <button               
+                onClick={switchCardBack}       
+                >Q2
+                </button>
+
+                <button               
+                onClick={switchCard}     
+                >Q1
+                </button>
+                {/* <button               
+                onClick={switchCard}     
+                >Q3
+                </button> */}
             </div>
             <div className="bottom">
               <dd>
