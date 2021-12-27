3e60640a9887034504d601f1680251419ffd3a0a
michaelfan0310 <michaelfan0310@gmail.com>
2021-12-26 20:44:22 -0500

    video2 Schind

--- src/components/VideoRoom2.js
+++ src/components/VideoRoom2.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
-import video2 from '../Video/Michael.mp4';
+import video1 from '../Video/Michael.mp4';
+import video2 from '../Video/Schind.mp4';
+
 
 function VideoRoom2(){
+    const [video, setVideo]=useState(video1);
+    const [toggle,setToggle]=useState(false);
+
+       function SwitchClip(){
+           setToggle(!toggle);
+           if(!toggle){
+           setVideo(video2);}else{
+               setVideo(video1);
+           }
+       }
     return(
         <div className="dictionary2">
         <div className="card2">
+
         
 
-       <video controls autoPlay loop muted className="video2" >
-          <source src={video2} type="video/mp4" >
-          </source>
+       <video controls autoPlay loop muted className="video2" 
+          src={video} type="video/mp4" />
+        
          <source src="movie.ogg" type="video/ogg">
         </source>
-        </video>
+
+        <button id="button2" onClick={SwitchClip}>Switch</button>      
        
         </div>
         </div>
